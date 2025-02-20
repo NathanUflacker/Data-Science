41844a761ed3555fcf081a3466a42210d2bfc02d
Nathan da Silveira Uflacker <nathanuflacker@hotmail.com>
2025-02-20 09:35:58 -0300

    Name_Corrections
    Made some corrections on titles

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 
 # st.write("Hello World")
 
-st.title("Instagram Analytcs")
+st.title("Instagram Analitycs")
+st.caption('Este site está em desenvolvimento em virtude de um projeto acadêmico.')
 # st.header("Muito massa")
 # st.text("Corpo do texto")
 # st.markdown("Clique aqui")
 
-uploaded_file = st.file_uploader('Dados')
+uploaded_file = st.file_uploader('Importe seus dados:')
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
 
     st.write(df.describe())
-    st.header('Analytcs')
+    st.header('Analitycs')
     st.write(df.head())
 
     fig, ax = plt.subplots(1,1)
@@ -23,4 +24,6 @@ if uploaded_file:
     ax.set_xlabel('Alcance')
     ax.set_ylabel('Curtidas')
     
-    st.pyplot(fig) # Figura, é o que manda
+    st.pyplot(fig) # Figura, é o que manda)
+
+    
